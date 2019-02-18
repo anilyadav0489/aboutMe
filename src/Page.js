bd5c0361dfb3ec3bdf4061183ff756bfe4aabacf
anilyadav0489 <anilyadav0489@gmail.com>
2019-02-18 23:32:25 +0530

    initial raw commit

---
+++ src/Page.js
@@ -0,0 +1,41 @@
+import React, {Component} from 'react';
+
+class Page extends Component {
+    render() {
+        return (
+            <div className="page">
+                <div className="my-picture"></div>
+                <div className="name-title">Anil Yadav</div>
+                <div className="work-title">Web Developer</div>
+                <div className="vertical-timeline"></div>
+                <div className="summary-title">Summary</div>
+                <div className="summary">Web developer and artist with 6+ years of experience in front-end development using React JS, object-oriented and user-centered design, seeks position with a top technology firm.</div>
+                <div className="experience-title-main">Experience</div>
+                <div className="designation-1-title">eprs</div>
+                <div className="company-1-title">eprs</div>
+                <div className="project-1-title">eprs</div>
+                <div className="project-1-detail">Web developer and artist with 6+ years of experience in front-end development using React JS, object-oriented and user-centered design, seeks position with a top technology firm.</div>
+                <div className="designation-2-title">eprs</div>
+                <div className="company-2-title">eprs</div>
+                <div className="project-2-title">eprs</div>
+                <div className="project-2-detail">Web developer and artist with 6+ years of experience in front-end development using React JS, object-oriented and user-centered design, seeks position with a top technology firm.</div>
+                <div className="designation-3-title">eprs</div>
+                <div className="company-3-title">eprs</div>
+                <div className="project-3-title">eprs</div>
+                <div className="project-3-detail">Web developer and artist with 6+ years of experience in front-end development using React JS, object-oriented and user-centered design, seeks position with a top technology firm.</div>
+                <div className="designation-4-title">eprs</div>
+                <div className="company-4-title">eprs</div>
+                <div className="project-4-title">eprs</div>
+                <div className="project-4-detail">Web developer and artist with 6+ years of experience in front-end development using React JS, object-oriented and user-centered design, seeks position with a top technology firm.</div>
+                <div className="personal-info-title">Personal Info</div>
+                <div className="address-title">Address</div>
+                <div className="address">Pune, Maharashtra - 412114</div>
+                <div className="skills-title"></div>
+                <div className="skills"></div>
+
+            </div>
+        );
+    }
+}
+
+export default Page;
