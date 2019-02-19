9b6db4a5c805167b51808e1e692183a9b41a6ef4
anilyadav0489 <anilyadav0489@gmail.com>
2019-02-20 01:14:18 +0530

    Added skills and education

--- src/Page.js
+++ src/Page.js
@@ -13,34 +13,120 @@ class Page extends Component {
                 <div className="vertical-timeline"></div>
                 <div className="summary-title">Summary</div>
                 <div className="summary">Web developer and artist with 6+ years of experience in front-end development using React JS, object-oriented and user-centered design, seeks position with a top technology firm.</div>
+                <div className="experience-icon"><i className="fa fa-business-time"></i></div>
                 <div className="experience-title-main">Experience</div>
+                <div className="designation-1-start-time">2018-09</div>
+                <div className="designation-1-end-time">Present</div>
                 <div className="designation-1-title">Sr. Associate (Team Lead)</div>
                 <div className="company-1-title">Cognizant</div>
                 <div className="project-1-title">Project COMET:</div>
                 <div className="project-1-detail">Working as front-end developer for banking application. Primarily using React JS and Java as the core technologies.
                                                     Application being deployed on AWS, helped me getting high level details of AWS.</div>
+                
+                <div className="designation-2-start-time">2016-12</div>
+                <div className="designation-2-end-time">2018-08</div>
                 <div className="designation-2-title">Associate Consultant</div>
                 <div className="company-2-title">Syntel</div>
                 <div className="project-2-title">Project: Electronic Payment Acceptance</div>
                 <div className="project-2-detail">Web developer and artist with 6+ years of experience in front-end development using React JS, object-oriented and user-centered design, seeks position with a top technology firm.</div>
+                <div className="designation-3-start-time">2013-06</div>
+                <div className="designation-3-end-time">2016-11</div>
                 <div className="designation-3-title">Associate Consultant</div>
                 <div className="company-3-title">Syntel</div>
                 <div className="project-3-title">Project: Enterprise Pricing Request System</div>
                 <div className="project-3-detail">A project for FedEx built on JSF's ice-faces and Java 6 helped me learn the basics of Java Programming, web designing using ice-faces and Javascript and how a good enterprise architecture helps in scalability of application. Worked as a core developer in a team of around 30 members and got visibility of almost all the aspects and modules of a large application.</div>
+                <div className="designation-4-start-time">2012-04</div>
+                <div className="designation-4-end-time">2013-05</div>
                 <div className="designation-4-title">Trainee</div>
                 <div className="company-4-title">Syntel</div>
                 <div className="project-4-title">Project: Interlink</div>
                 <div className="project-4-detail">A project for a Insurance based company Allstate. I started working as a junior developer and learnt Java and SQL with the domain knowledge about what insurance is, some key terms like Policies, endorsements, types of insurances and all.</div>
+                <div className="education-title-main">Education</div>
+                <div className="education-1-start-time">2007-07</div>
+                <div className="education-1-end-time">2011-06</div>
+                <div className="education-1-title">Bachelor of Technology (Computer Science)</div>
+                <div className="education-1-school">B. Tech in Computer Science (72%)</div>
+                <div className="education-2-start-time">2005-04</div>
+                <div className="education-2-end-time">2006-03</div>
+                <div className="education-2-title">Kendriya Vidyalaya, Kanpur</div>
+                <div className="education-2-school">Intermediate in Science stream (77%)</div>
                 <div className="personal-info-title">Personal Info</div>
                 <div className="address-title">Address</div>
                 <div className="address">Pune, Maharashtra - 412114</div>
-                <div className="skills-title"></div>
-                <div className="skills"></div>
+                <div className="phone-title">Phone</div>
+                <div className="phone">+91-84597-32047</div>
+                <div className="email-title">E-Mail</div>
+                <div className="email">AnilYadav0489@gmail.com</div>
+                <div className="dob-title">Date of Birth</div>
+                <div className="dob">24 April 1989</div>
+                <div className="profile-title">Profile</div>
+                <div className="profile">https://anil-yadav.herokuapp.com</div>
+                <div className="linkedin-title">LinkedIn Profile</div>
+                <div className="linkedin">xxx</div>
+                <div className="skills-title">Skills</div>
+                <div className="skill-1">ReactJS, Redux</div>
+                <div className="skill-1-bullets">
+                    <span className="dark"></span>
+                    <span className="dark"></span>
+                    <span className="dark"></span>
+                    <span className="dark"></span>
+                    <span className="light"></span>
+                </div>
+                <div className="skill-2">Javascript, NodeJS</div>
+                <div className="skill-2-bullets">
+                    <span className="dark"></span>
+                    <span className="dark"></span>
+                    <span className="dark"></span>
+                    <span className="dark"></span>
+                    <span className="light"></span>
+                </div>
+                <div className="skill-3">HTML5, CSS</div>
+                <div className="skill-3-bullets">
+                    <span className="dark"></span>
+                    <span className="dark"></span>
+                    <span className="dark"></span>
+                    <span className="dark"></span>
+                    <span className="light"></span>
+                </div>
+                <div className="skill-4">Java</div>
+                <div className="skill-4-bullets">
+                    <span className="dark"></span>
+                    <span className="dark"></span>
+                    <span className="dark"></span>
+                    <span className="light"></span>
+                    <span className="light"></span>
+                </div>
+                <div className="skill-5">Agile, Git Hub, SVN</div>
+                <div className="skill-5-bullets">
+                    <span className="dark"></span>
+                    <span className="dark"></span>
+                    <span className="light"></span>
+                    <span className="light"></span>
+                    <span className="light"></span>
+                </div>
+                <div className="skill-6">Amazon Web Services</div>
+                <div className="skill-6-bullets">
+                    <span className="dark"></span>
+                    <span className="dark"></span>
+                    <span className="light"></span>
+                    <span className="light"></span>
+                    <span className="light"></span>
+                </div>
+                <div className="skill-7">Unix</div>
+                <div className="skill-7-bullets">
+                    <span className="dark"></span>
+                    <span className="dark"></span>
+                    <span className="light"></span>
+                    <span className="light"></span>
+                    <span className="light"></span>
+                </div>
                 <div className="bullet-summary"></div>
                 <div className="bullet-experience-1"></div>
                 <div className="bullet-experience-2"></div>
                 <div className="bullet-experience-3"></div>
                 <div className="bullet-experience-4"></div>
+                <div className="bullet-education-1"></div>
+                <div className="bullet-education-2"></div>
 
             </div>
         );
